02586e003b936bd0042ee97e1bdbc30b4f0f3b3d
Snehasis.Parida <Snehasis.Parida@NOD-WIN-6s024.macmill.com>
2020-08-12 22:12:32 +0530

    Setup Commit

---
+++ GreatMinds/src/main/java/GreatMinds/GreatMinds/App.java
@@ -0,0 +1,13 @@
+package GreatMinds.GreatMinds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
